413bc5cebb24e3c721acf6a21e4c957d980a5300
sergiima <s.matyash@list.ru>
2015-09-22 13:50:23 +0300

    Added popup+overlay

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var whoresContainer = document.getElementById('whores-container');
+$(document).ready(function() {
 
-for(i=0; i<whores.length; i++) {
-   var whoresDiv = '<h3>' + whores[i].name + '</h3>';
-   whoresContainer.innerHTML += '<div class="item"><img src="http://placekitten.com.s3.amazonaws.com/homepage-samples/200/286.jpg">' + whoresDiv + '</div>';
- }
+    var whoresContainer = document.getElementById('whores-container');
+
+    for(i=0; i<whores.length; i++) {
+       var whoresInnerContainer = '<h3>' + whores[i].name + '</h3>';
+       whoresContainer.innerHTML += '<div class="item"><a href="#popup"><img src="http://placekitten.com.s3.amazonaws.com/homepage-samples/200/286.jpg"></a><a href="#popup">' + whoresInnerContainer + '</a></div>';
+    }
+
+    $('a').click(function () {
+    	$('#overlay').addClass('open');
+    	$('#popup').addClass('open');
+    });
+    $('#overlay').click(function () {
+    	$('#overlay').removeClass('open');
+    	$('#popup').removeClass('open');
+    });
+});
+
+
+
+
 
 
